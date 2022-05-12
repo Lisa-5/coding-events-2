f3e8766013e6aa103a11f795195f5f5f1091a524
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-05-11 20:04:51 -0500

    prepping to sart to work on Exercises for Chapter 13

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+   // private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Coding & Cocktails: The Garnish | Introduction to CSS", "Join us for our monthly coding series, Coding & Cocktails. Women are invited to attend sessions to get an introduction to website building in a fun, laid-back atmosphere");
+        events.put("SHIFT Tech Transformation Summit", "A day of virtual sessions to learn from industry leaders from Centene, Broadcom, Mastercard, Penn Interactive, NGA, and others as they discuss the future of talent in the tech industry." );
+        events.put("gRPC A new Rest replacement ?", "Tom Collins of First Street Software will demonstrate gRPC." +
+                "Its the new hotness to replace or work with REST. So think of it as binary REST calls. They are using it as a binary communication for microservices as well. VS2022 makes it pretty easy really.");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,8 +36,8 @@ public class EventController {
 
     // this method lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String description){
+        events.put(eventName, description);
         return "redirect:/events";
     }
 
